7b81833f39ac64bea89f64205ef77d8c5df2ba17
Matheus Moraes <107554927+matheusgmo@users.noreply.github.com>
2024-10-31 17:36:27 -0300

    login_page
    Criei a pagina de login e cadastro e comecei a pagina de mais informações sobre o filme

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
 const searchPage = document.getElementById('searchPage')
 const searchInput = document.getElementById('searchInput')
 
-const dBlock = () => {
+const dBlockSearch = () => {
     searchPage.classList.remove('d-none')
     searchPage.classList.add('d-block')
     searchInput.focus()
 }
 
-const dNone = () => {
+const dNoneSearch = () => {
     searchPage.classList.remove('d-block')
     searchPage.classList.add('d-none')
     searchInput.value = ""
@@ -27,44 +27,47 @@ const indisponivel = () => {
 
 // Consumindo a api
 
-const getFilmList = (apiUrl) => {
-    const apiKey = '?api_key=f1d01bea3ef891d53eaf4b9c2e492281'
-    return fetch(apiUrl+apiKey)
-    .then(response => {
-        if (!response.ok) throw new Error('Erro ao buscar dados')
-        return response.json()
+const main = document.getElementById('main')
+
+if(main){
+    const getFilmList = (apiUrl) => {
+        const apiKey = '?api_key=f1d01bea3ef891d53eaf4b9c2e492281'
+        return fetch(apiUrl+apiKey)
+        .then(response => {
+            if (!response.ok) throw new Error('Erro ao buscar dados')
+            return response.json()
+        })
+        .then(data => data)
+        .catch(error => console.error('Erro:', error))
+    }
+    
+    
+    const getUrl = (array, index, posterUrl, width = 200) =>  `${posterUrl}w${width}${array[index].poster_path}`
+    
+    getFilmList('https://api.themoviedb.org/3/movie/popular')
+    .then(data => {
+        const poster = document.getElementById('poster')
+        poster.innerHTML += `<img 
+        src='${getUrl(data.results, 0,'https://image.tmdb.org/t/p/', 400)}' 
+        alt="Capa do filme" class="w-100">`
+    
+        const filmesEmAlta = document.getElementById('filmesEmAlta')
+        for (let i = 0; i < data.results.length; i++) {
+            filmesEmAlta.innerHTML += `<img 
+            src='${getUrl(data.results, i, 'https://image.tmdb.org/t/p/')}' 
+            alt="Capa do filme" 
+            onclick="indisponivel()">`
+        }
     })
-    .then(data => data)
-    .catch(error => console.error('Erro:', error))
+    
+    getFilmList('https://api.themoviedb.org/3/tv/popular')
+    .then(data => {
+        const seriesEmAlta = document.getElementById('seriesEmAlta')
+        for (let i = 0; i < data.results.length; i++) {
+            seriesEmAlta.innerHTML += `<img 
+            src='${getUrl(data.results, i, 'https://image.tmdb.org/t/p/')}' 
+            alt="Capa do filme" 
+            onclick="indisponivel()">`
+        }
+    })
 }
-
-
-const getUrl = (array, index, posterUrl, width = 200) =>  `${posterUrl}w${width}${array[index].poster_path}`
-
-getFilmList('https://api.themoviedb.org/3/movie/popular')
-.then(data => {
-    const poster = document.getElementById('poster')
-    poster.innerHTML += `<img 
-    src='${getUrl(data.results, 0,'https://image.tmdb.org/t/p/', 400)}' 
-    alt="Capa do filme" class="w-100" 
-    onclick="indisponivel()">`
-
-    const filmesEmAlta = document.getElementById('filmesEmAlta')
-    for (let i = 0; i < data.results.length; i++) {
-        filmesEmAlta.innerHTML += `<img 
-        src='${getUrl(data.results, i, 'https://image.tmdb.org/t/p/')}' 
-        alt="Capa do filme" 
-        onclick="indisponivel()">`
-    }
-})
-
-getFilmList('https://api.themoviedb.org/3/tv/popular')
-.then(data => {
-    const seriesEmAlta = document.getElementById('seriesEmAlta')
-    for (let i = 0; i < data.results.length; i++) {
-        seriesEmAlta.innerHTML += `<img 
-        src='${getUrl(data.results, i, 'https://image.tmdb.org/t/p/')}' 
-        alt="Capa do filme" 
-        onclick="indisponivel()">`
-    }
-})
