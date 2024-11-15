b4ea235a78a5a1e4fb1f0f0f9fec0a925b4f190a
Matheus Moraes <107554927+matheusgmo@users.noreply.github.com>
2024-11-15 19:03:47 -0300

    detalhes
    adicionei uma pagina de detalhes dos filmes

--- scripts.js
+++ scripts.js
@@ -27,47 +27,78 @@ const indisponivel = () => {
 
 // Consumindo a api
 
-const main = document.getElementById('main')
-
-if(main){
-    const getFilmList = (apiUrl) => {
-        const apiKey = '?api_key=f1d01bea3ef891d53eaf4b9c2e492281'
-        return fetch(apiUrl+apiKey)
-        .then(response => {
-            if (!response.ok) throw new Error('Erro ao buscar dados')
-            return response.json()
-        })
-        .then(data => data)
-        .catch(error => console.error('Erro:', error))
-    }
-    
-    
-    const getUrl = (array, index, posterUrl, width = 200) =>  `${posterUrl}w${width}${array[index].poster_path}`
-    
-    getFilmList('https://api.themoviedb.org/3/movie/popular')
-    .then(data => {
-        const poster = document.getElementById('poster')
-        poster.innerHTML += `<img 
-        src='${getUrl(data.results, 0,'https://image.tmdb.org/t/p/', 400)}' 
-        alt="Capa do filme" class="w-100">`
-    
-        const filmesEmAlta = document.getElementById('filmesEmAlta')
-        for (let i = 0; i < data.results.length; i++) {
-            filmesEmAlta.innerHTML += `<img 
-            src='${getUrl(data.results, i, 'https://image.tmdb.org/t/p/')}' 
-            alt="Capa do filme" 
-            onclick="indisponivel()">`
-        }
+const getFilmList = (apiUrl) => {
+    const apiKey = '?api_key=f1d01bea3ef891d53eaf4b9c2e492281'
+    return fetch(apiUrl+apiKey)
+    .then(response => {
+        if (!response.ok) throw new Error('Erro ao buscar dados')
+        return response.json()
     })
-    
-    getFilmList('https://api.themoviedb.org/3/tv/popular')
-    .then(data => {
-        const seriesEmAlta = document.getElementById('seriesEmAlta')
-        for (let i = 0; i < data.results.length; i++) {
-            seriesEmAlta.innerHTML += `<img 
-            src='${getUrl(data.results, i, 'https://image.tmdb.org/t/p/')}' 
-            alt="Capa do filme" 
-            onclick="indisponivel()">`
-        }
-    })
+    .then(data => data)
+    .catch(error => console.error('Erro:', error))
 }
+
+
+const getUrl = (array, index, posterUrl, width = 200) =>  `${posterUrl}w${width}${array[index].poster_path}`
+
+getFilmList('https://api.themoviedb.org/3/movie/popular')
+.then(data => {
+    const poster = document.getElementById('poster')
+    poster.innerHTML += `<img 
+    src='${getUrl(data.results, 0,'https://image.tmdb.org/t/p/', 400)}' 
+    alt="Capa do filme" class="w-100">`
+    poster.href += `#${data.results[0].id}`
+
+    const filmesEmAlta = document.getElementById('filmesEmAlta')
+    for (let i = 0; i < data.results.length; i++) {
+        filmesEmAlta.innerHTML += `
+        <a href="detalhes.html#${data.results[i].id}">
+            <img src='${getUrl(data.results, i, 'https://image.tmdb.org/t/p/')}' alt="Capa do filme">
+        </a>
+        `
+    }
+
+})
+
+getFilmList('https://api.themoviedb.org/3/tv/popular')
+.then(data => {
+
+    console.log(data.results[0])
+
+    const seriesEmAlta = document.getElementById('seriesEmAlta')
+    for (let i = 0; i < data.results.length; i++) {
+        seriesEmAlta.innerHTML += `
+        <a href="detalhes.html#${data.results[i].id}">
+        <img src='${getUrl(data.results, i, 'https://image.tmdb.org/t/p/')}' alt="Capa do filme">
+        </a>
+        `
+    }
+})
+
+const id = window.location.hash.slice(1)
+
+getFilmList(`https://api.themoviedb.org/3/movie/${id}`)
+.then(data => {
+    const detalhes = document.getElementById('detalhes');
+    detalhes.innerHTML = `
+    <img src="https://image.tmdb.org/t/p/w400${data.backdrop_path}" alt="${data.title || "Imagem indisponível"}" style="width: 100%">
+    <div class="container-fluid">
+        <h1 class="mt-3 mb-3 display-4">${data.title || "Título não encontrado"}</h1>
+        <p>${data.overview || "Descrição não disponível"}</p>
+        <p><i class="bi-star-half"></i> ${data.vote_average} - ${data.vote_count} votes</p>
+    </div>
+    `
+})
+
+getFilmList(`https://api.themoviedb.org/3/tv/${id}`)
+.then(data => {
+    const detalhes = document.getElementById('detalhes');
+    detalhes.innerHTML = `
+    <img src="https://image.tmdb.org/t/p/w400${data.backdrop_path}" alt="${data.title || "Imagem indisponível"}" style="width: 100%">
+    <div class="container-fluid">
+        <h1 class="mt-3 mb-3 display-4">${data.name || "Título não encontrado"}</h1>
+        <p>${data.overview || "Descrição não disponível"}</p>
+        <p><i class="bi-star-half"></i> ${data.vote_average} - ${data.vote_count} votes</p>
+    </div>
+    `
+})
